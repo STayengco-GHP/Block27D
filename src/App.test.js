10ec5cf1ec71f93963fba7280733b5885ab925e0
STayengco <stephanie@zhoug>
2023-08-16 13:27:58 -0700

    missing tests added

--- src/App.test.js
+++ src/App.test.js
@@ -10,10 +10,8 @@ describe("App component", () => {
   test("displays the details of a featured puppy when clicked", () => {
     // Render the App component
     render(<App />);
-
     // Find the puppy with name "Sir Waggington"
     const puppyName = screen.getByText("Sir Waggington");
-
     // Simulate a click on the puppy
     fireEvent.click(puppyName);
 
@@ -35,28 +33,56 @@ describe("App component", () => {
   // TODO
   test("does not display the details of a featured puppy initially", () => {
     // Render the App component
+    render(<App />);
     // Assert that the featured puppy's name is not displayed
+    const puppyHeading = screen. queryByRole ("heading", {
+    hidden: true, });
+    expect (puppyHeading).not.toBeInTheDocument () ;
     // Assert that the featured puppy's age is not displayed
+    const featuredPuppyAge = screen.queryByText("Age");
+    expect(featuredPuppyAge).not.toBeInTheDocument();
     // Assert that the featured puppy's email is not displayed
+    const featuredPuppyEmail = screen.queryByText("Email");
+    expect(featuredPuppyEmail).not.toBeInTheDocument();
   });
 
   // TODO
   test("does not change the featured puppy when the same puppy is clicked twice", () => {
     // Render the App component
+    render(<App />);
     // Find the puppy with name "Sir Waggington"
+    const puppyName = screen.getByText("Sir Waggington");
     // Simulate a click on the puppy
+    fireEvent.click(puppyName);
     // Assert that the initial featured puppy's name is displayed
+    const featuredPuppyName = screen.getByRole("heading", {
+      name: /Sir Waggington/i,
+    });
+    expect(featuredPuppyName).toBeInTheDocument();
     // Simulate another click on the same puppy
+    fireEvent.click(puppyName);
     // Assert that the featured puppy's name is still displayed and is the same as the initial featured puppy
+    expect(featuredPuppyName).toBeInTheDocument();
   });
 
   // TODO
   test("displays the details of a featured puppy when clicked", () => {
     // Render the App component
+    render(<App />);
     // Find the puppy with name "Miss Furbulous"
+    const puppyName = screen.getByText("Miss Furbulous");
     // Simulate a click on the puppy
+    fireEvent.click(puppyName);
     // Assert that the featured puppy's name is displayed
+    const featuredPuppyName = screen.getByRole("heading", {
+      name: /Miss Furbulous/i,
+    });
+    expect(featuredPuppyName).toBeInTheDocument();
     // Assert that the featured puppy's age is displayed
+    const featuredPuppyAge = screen.getByText(/Age: \d+/);
+    expect(featuredPuppyAge).toBeInTheDocument();
     // Assert that the featured puppy's email is displayed
+    const featuredPuppyEmail = screen.getByText(/Email: \S+/);
+    expect(featuredPuppyEmail).toBeInTheDocument();
   });
 });
